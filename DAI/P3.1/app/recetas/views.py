bb0bde8469588f4636330793c49319292f1d6362
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2022-11-03 17:46:23 +0100

    terminada la P3.1

--- DAI/P3.1/app/recetas/views.py
+++ DAI/P3.1/app/recetas/views.py
@@ -1,6 +1,6 @@
 from curses.ascii import HT
 import re
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse
 
 def index(request):
     return HttpResponse('Hello World!')
