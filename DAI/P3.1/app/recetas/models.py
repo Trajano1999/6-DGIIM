f0b1ba5b4b0eb613de765635f77571a743bb3bcc
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2022-11-17 13:11:08 +0100

    avances en P3.2

--- DAI/P3.1/app/recetas/models.py
+++ DAI/P3.1/app/recetas/models.py
@@ -4,7 +4,7 @@ class Receta(models.Model):
   nombre       = models.CharField(max_length=200)
   preparación  = models.TextField(max_length=5000)
   foto         = models.FileField(upload_to='media/fotos')
-  ... 
+  ...
   
   def __str__(self):
     return self.nombre
