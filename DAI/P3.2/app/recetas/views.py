476cbb0c959d4fe6f8152f228768b5b02d807c17
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2022-11-17 14:33:18 +0100

    mas avances en P3.2

--- DAI/P3.2/app/recetas/views.py
+++ DAI/P3.2/app/recetas/views.py
@@ -4,7 +4,7 @@ from recetas.models import Receta
 def index(request):
     if request.GET.get('searchInput') != None:
         busquedas = Receta.objects.filter(nombre = request.GET.get('searchInput'))
-        return render(request, 'lista_recetas.html', {'busquedas': busquedas})
+        return render(request, 'index.html', {'busquedas': busquedas})
     else:
         busquedas = Receta.objects.all()
         return render(request, "index.html", {'busquedas': busquedas})
