782ded8fd2ef9de8e5c41c94175b368dfba77745
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2022-11-28 15:45:18 +0100

    terminada la practica P3.3

--- DAI/P3.3/app/recetas/models.py
+++ DAI/P3.3/app/recetas/models.py
@@ -1,10 +1,21 @@
-# ¡ al modificar los campos de este archivo debemos migrar !
+# ¡ Al modificar los campos de este archivo debemos migrar !
 
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+import re
   
+def validate_mayus(valor):
+	result = re.match('[A-Z]+.*', valor)
+	if result == None:
+		raise ValidationError(
+			_('%(valor)s debe comenzar por mayúscula'),
+			params={'valor': valor}
+		)
+
 class Receta(models.Model):
-	nombre       = models.CharField(max_length=200)
-	preparación  = models.TextField(max_length=5000)
+	nombre       = models.CharField(max_length=200, validators=[validate_mayus])
+	preparación  = models.TextField(max_length=5000, validators=[validate_mayus])
 	foto         = models.FileField(upload_to='media/fotos')
   
 	def __str__(self):
